61434419337ea25630cd69b3f0f586fe2310ce8e
Jason <you@example.com>
2025-03-30 18:55:03 -0400

    Changed settings back.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6hti1r9n#5joqc6367wjh=pxsdq!@@7k%7847&3nf0ighrxhy('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
